704cbbed65a872ea404ab78dd4fcaa6736bf4749
Eric Chan <ericchan@macbook-pro.local>
2016-02-26 01:27:53 +0800

    Initial Commit

---
+++ mark_six/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  mark_six
+//
+//  Created by Eric Chan on 16/2/26.
+//  Copyright © 2016年 Eric Chan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
